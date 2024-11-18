1ae28d34ca3f5f216d85ddd5e81c1298ddbe29ce
Dan Rikster Abangan <danriksterabangan@gmail.com>
2024-11-19 06:14:05 +0800

    Sidebar Error Fixed. Hire Button in Progress.

--- resources/views/front/account/sidebar.blade.php
+++ resources/views/front/account/sidebar.blade.php
@@ -1,57 +1,63 @@
 <div class="card border-0 shadow mb-4 p-3">
     <div class="s-body text-center mt-3">
+        @if(Auth::check())
+            @if(!empty(Auth::user()->image))
+                <img src="{{ asset('profile_pic/thumb/'.Auth::user()->image) }}" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
+            @else
+                <img src="{{ asset('assets/images/avatar7.png') }}" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
+            @endif
 
-        @if(Auth::user()->image != '')
-            <img src="{{ asset('profile_pic/thumb/'.Auth::user()->image) }}" alt="avatar"  class="rounded-circle img-fluid" style="width: 150px;">
+            <h5 class="mt-3 pb-0">{{ Auth::user()->firstName }} {{ Auth::user()->lastName }}</h5>
+            <p class="text-muted mb-1 fs-6">{{ Auth::user()->designation ?? 'No designation provided' }}</p>
+            <div class="d-flex justify-content-center mb-2">
+                <button data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" class="btn btn-primary">Change Profile Picture</button>
+            </div>
         @else
-            <img src="{{ asset('assets/images/avatar7.png') }}" alt="avatar"  class="rounded-circle img-fluid" style="width: 150px;">
+            <p class="text-center text-muted">You need to log in to view this section.</p>
         @endif
+    </div>
+</div>
 
-        <h5 class="mt-3 pb-0">{{ Auth::user()->firstName }} {{ Auth::user()->lastName }}</h5>
-        <p class="text-muted mb-1 fs-6">{{ Auth::user()->designation }}</p>
-        <div class="d-flex justify-content-center mb-2">
-            <button data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" class="btn btn-primary">Change Profile Picture</button>
+@if(Auth::check())
+    <div class="card account-nav border-0 shadow mb-4 mb-lg-0">
+        <div class="card-body p-0">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item d-flex justify-content-between p-3">
+                    <a href="{{ route('account.show', ['id' => Auth::user()->id]) }}">Me</a>
+                </li>
+                <li class="list-group-item d-flex justify-content-between p-3">
+                    <a href="{{ route('account.profile') }}">Profile Settings</a>
+                </li>
+                <li class="list-group-item d-flex justify-content-between p-3">
+                    <a href="{{ route('account.accountPassword') }}">Change Password</a>
+                </li>
+
+                {{-- Client and Admin Sidebar --}}
+                @if (Auth::user()->role == 'user' || Auth::user()->role == 'admin')
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                        <a href="{{ route('account.createJob') }}">Post a Job</a>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                        <a href="{{ route('account.myJobs') }}">My Jobs</a>
+                    </li>
+                @endif           
+
+                {{-- Freelancer and Admin Sidebar --}}
+                @if (Auth::user()->role == 'freelancer' || Auth::user()->role == 'admin')
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                        <a href="{{ route('freelancer.verify-now') }}">Verify Now</a>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                        <a href="{{ route('account.myJobApplications') }}">Jobs Applied</a>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                        <a href="{{ route('account.savedJobs') }}">Saved Jobs</a>
+                    </li>
+                @endif
+                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                    <a href="{{ route('account.logout') }}">Logout</a>
+                </li>                                                        
+            </ul>
         </div>
     </div>
-</div>
-<div class="card account-nav border-0 shadow mb-4 mb-lg-0">
-    <div class="card-body p-0">
-        <ul class="list-group list-group-flush ">
-            <li class="list-group-item d-flex justify-content-between p-3">
-                <a href="{{ route('account.show', ['id' => Auth::user()->id]) }}">Me</a>
-            </li>
-            <li class="list-group-item d-flex justify-content-between p-3">
-                <a href="{{ route('account.profile') }}">Profile Settings</a>
-            </li>
-            <li class="list-group-item d-flex justify-content-between p-3">
-                <a href="{{ route('account.accountPassword') }}">Change Password</a>
-            </li>
-
-            {{-- Client and Admin SideBar --}}
-            @if (Auth::user()->role == 'user' || Auth::user()->role == 'admin')
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                    <a href="{{ route('account.createJob') }}">Post a Job</a>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                    <a href="{{ route('account.myJobs') }}">My Jobs</a>
-                </li> 
-            @endif           
-            
-            {{-- Freelancer and Admin SideBar --}}
-            @if (Auth::user()->role == 'freelancer' || Auth::user()->role == 'admin')
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                    <a href="{{ route('freelancer.verify-now') }}">Verify Now</a>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                    <a href="{{ route('account.myJobApplications') }}">Jobs Applied</a>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                    <a href="{{ route('account.savedJobs') }}">Saved Jobs</a>
-                </li>
-            @endif
-            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                <a href="{{ route('account.logout') }}">Logout</a>
-            </li>                                                        
-        </ul>
-    </div>
-</div>
+@endif
